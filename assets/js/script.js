0aead5ccab22d7d748d85142726f16edd8188e16
MuhammadRizwan89 <mrizwansaeed89xclusive@gmail.com>
2023-08-09 19:35:32 +0500

    javascript ist assigment push code

---
+++ assets/js/script.js
@@ -0,0 +1,153 @@
+// ============chapter 1 -Q1=============
+
+// let a = "Hellow there! well come to aur website"
+// alert(a);
+
+// ============Q-02=============
+
+// let b = "Error! Please enter a valid password.";
+// alert(b);
+
+// ============Q-03=============
+
+// let c = "WEL COME TO JS LAND....\n HAPPY CODDING";
+// alert(c)
+
+// ============Q-04=============
+// alert("Well Come to JS Land")
+// alert("Happy Coding!")
+
+// ============Q-05=============
+// let hello = "Hello... I can run JS through my web browser'console"
+// console.log(hello);
+
+// ============Chapter 2 Q-01=============
+// let userName = "Muhammad Rizwan";
+// let age =  "30year"
+// let certified = "Certified Moble Application Development";
+// alert(userName);
+// alert(age);
+// alert(certified);
+
+// ============Chapter 2 Q-02=============
+// alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// ============Chapter 2 Q-03=============
+// alert("My email address is example@example.com");
+
+// ============Chapter 2 Q-04=============
+// alert("I am trying to learn from Book A smarter way to learn JavaScript");
+
+// ============Chapter 2 Q-05=============
+// document.write("Yah! I can write HTML content through JavaScrip");
+
+// ============Chapter 3 Q-01=============
+
+// Get the visitor's age using the prompt function
+// const visitorAge = prompt("What is your age?");
+
+// ============Chapter 3 Q-02=============
+
+// if (visitorName = ("Muhammad Rizwan")) {
+//   alert("Hello, " + visitorName + "! Welcome to our website!");
+// } else {
+//   alert("Hello there! Welcome to our website!");
+// }
+
+// ============Chapter 3 Q-02=============
+
+// let visitorName = "john Doe";
+// let productTitle = "T-shirt";
+// let quantity = 5;
+// let store = "xyz clothing store";
+// document.write(visitorName + " ordered " + quantity + productTitle + " on " + store);
+
+// ============Chapter 4 Q-01=============
+
+// let variable3 = "type of variables "+"let,const,var"
+// document.write(variable3)
+
+// ============Q-02=============
+
+// let legalVariable = "LEGAL VARIABLE "+"pakistan, _karachi, $lahore, happyCodding, seaView "
+// let illLegalVariable = "ILL LEGAL VARIABLE "+" 1a,fist-flour,function,@username,let"
+
+// document.write(legalVariable+illLegalVariable)
+
+//  ============Q-03=============
+// document.write("Variable names in javaScript can only contain letters,number,underscores,and dollor sign$")
+
+//  ============Q-03=============
+// document.write("variable must begin with a letter, underscore or dollarSign in javaScript ")
+
+// ============Chapter 5 Q-01=============
+
+// let num1 = 5;
+// let num2 = 3;
+// let c = num1 + num2;
+// document.write(c);
+
+// ============Chapter 5 Q-02=============
+
+// let a = (600)
+// let b = a*4
+// document.write(a + b)
+// document.write(" eish ticket 600 and i get 5 ticket 3000 pkr ")
+
+// ============Chapter 5 Q-03=============
+
+// document.write("<h1>Table of 4</h1>")
+// let x = 4
+// document.write("4x1="+x*1)
+// document.write("</br>4x1="+x*2)
+// document.write("</br>4x1="+x*3)
+// document.write("</br>4x1="+x*4)
+// document.write("</br>4x1="+x*5)
+// document.write("</br>4x1="+x*6)
+// document.write("</br>4x1="+x*7)
+// document.write("</br>4x1="+x*8)
+// document.write("</br>4x1="+x*9)
+// document.write("</br>4x1="+x*10)
+
+// ============Chapter 5 Q-04=============
+
+function convert(){
+  let c = document.getElementById("data").value;
+  let f = (c * 1.8) + 32;
+  document.getElementById("res").innerText = "${c} C = ${f} F"
+}
+
+
+// ============Chapter 5 Q-05=============
+
+
+
+
+
+
+
+// ============Q-01=============
+// function a() {
+//   alert("Hellow there! well come to aur website");
+// }
+// window.onload = a;
+
+// ============Q-02=============
+// function a() {
+//   var a = document.getElementById("errorMessage");
+//   a.innerText = "Error! Please enter a valid password.";
+// }
+
+// a();
+
+// ============Q-03=============
+// function dispalymessage() {
+//   var a = document.getElementById("jscoding");
+//   var b = "WEL COME TO JS LAND....";
+//   var c = "HAPPY CODDING";
+
+//   a.innerText = "WEL COME TO JS LAND....\n HAPPY CODDING "
+
+// }
+
+// dispalymessage();
